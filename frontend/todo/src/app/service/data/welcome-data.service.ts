418d721098334965756a01be472f9239461e31fe
vyom-shah <vyom0123@gmail.com>
2020-06-04 15:21:32 -0400

    Create welcome-data.service.ts

---
+++ frontend/todo/src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+export class HelloWorldBean {
+  constructor(public message:string){ }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WelcomeDataService {
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  executeHelloWorldBeanService() {
+    return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world-bean');
+    //console.log("Execute Hello World Bean Service")
+  }
+  //http://localhost:8080/hello-world/path-variable/vyom
+
+  executeHelloWorldServiceWithPathVariable(name) {
+    // let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+
+    // let headers = new HttpHeaders({
+    //     Authorization: basicAuthHeaderString
+    //   })
+
+    return this.http.get<HelloWorldBean>(
+      `http://localhost:8080/hello-world/path-variable/${name}`,
+      //{headers}
+      );
+    //console.log("Execute Hello World Bean Service")
+  }
+}
